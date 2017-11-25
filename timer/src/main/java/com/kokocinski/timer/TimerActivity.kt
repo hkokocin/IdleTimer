bfb594fd8e463d83c8edb4c87c6bfc6478d55b4c
Hendrik Kokocinski <hendrik.kokocinski@posteo.de>
2017-11-25 15:44:54 +0100

    project structure

---
+++ timer/src/main/java/com/kokocinski/timer/TimerActivity.kt
@@ -0,0 +1,12 @@
+package com.kokocinski.timer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TimerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.timer_activity)
+    }
+}
---
+++ timerlist/src/main/java/com/kokocinski/timerlist/TimerListActivity.kt
@@ -0,0 +1,12 @@
+package com.kokocinski.timerlist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TimerListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer_list)
+    }
+}
